fc93518b11872196607fd0a4e39822278af3a440
Thomas Huebscher <hubscherthomas@yahoo.de>
2021-02-08 16:18:38 +0100

    created a dummy encoding function for the dataframe and created and stored a model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.metrics import confusion_matrix
 
 import dataInfo
 import plot
@@ -26,19 +27,29 @@ ordinal_encoder = OrdinalEncoder(categories=[['e', 'p']])
 ordinal_encoder.fit(X[['class']])
 y = pd.DataFrame(ordinal_encoder.transform(X[['class']])) #our target variable
 
-plot.target(X)
-
-plot.count(X)
+#plot.target(X)
+#plot.count(X)
 #### with help of the plot we see some information for further preprocessing
+print(X['veil-type'].unique())   # just one unique value. Variable can be dropped
+X.drop('veil-type', axis = 1, inplace=True)
 
 X.drop('class', axis = 1, inplace=True) # delte target from dataset
 
+X = preprocess.dummies(X)
+print('len',len(X.columns))
+
 X_cardinality = preprocess.cardinality(X)
-print(X_cardinality.columns)
+#print(X_cardinality.columns)
 X_train, X_valid, y_train, y_valid = train_test_split(X_cardinality, y, train_size=0.8, test_size=0.2,random_state=0)
 
 model = model.logReg()
-#model.fit(X_train, y_train)
-#predictions = model.predict(X_valid)
-#model.score(X_valid,y_valid)
+model.fit(X_train, y_train)
+predictions = model.predict(X_valid)
+print('\n Logistic Regression Score', model.score(X_valid,y_valid))
+print(confusion_matrix(y_valid, predictions))
+
+############################################
+#we want to predict the model with less values.
+#With help of the plots we can choose some important variables to predict the target...
+
 
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
 # a module for preprocessing the data
 
+import pandas as pd
+
 def cardinality(X):
     '''This function gets a DataFrame and return only those columns with Cardinality < 2 (binary)'''
     x_binary_cols= [cname for cname in X.columns if X[cname].nunique() < 10]
@@ -9,5 +11,11 @@ def cardinality(X):
     X.drop(non_binary_cols, axis = 1, inplace=True) # delte non_binary columns from dataset
     return X
 
-def oneHot(X):
-    pass
+def dummies(X):
+        X = pd.get_dummies(X, columns=['cap-shape', 'cap-surface', 'bruises', 'odor', 'gill-attachment',
+       'gill-spacing', 'gill-size', 'stalk-shape', 'stalk-root',
+       'stalk-surface-above-ring', 'stalk-surface-below-ring',
+       'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-color',
+       'ring-number', 'ring-type', 'spore-print-color', 'population',
+       'habitat'], drop_first=True)
+        return X
